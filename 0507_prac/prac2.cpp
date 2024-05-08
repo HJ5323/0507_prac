4ec346fbbdeff8a4f9bcc6b8f405681dd29a883e
HJ5323 <167291731+HJ5323@users.noreply.github.com>
2024-05-08 15:20:49 +0900

    실습2.회원명부 로그인_ver.sstream

--- 0507_prac/prac2.cpp
+++ 0507_prac/prac2.cpp
@@ -1,68 +1,68 @@
 // 실습 2. 회원 명부를 응용한 로그인 기능
 //1. 사용자에게 "이름을 입력하세요." 라는 메세지를 출력한 뒤 이름 입력 받기
 //2. 사용자에게 "비밀번호를 입력하세요." 라는 메세지를 출력한 뒤 비밀번호 입력 받기
-//3. member.txt에서 한줄씩 "이름"과 "비번"을 검사하여 로그인 성공시 "로그임 성공" 실패 시 "로그인 실패" 출력
+//3. member.txt에서 한줄씩 "이름"과 "비번"을 검사하여 로그인 성공시 "로그인 성공" 실패 시 "로그인 실패" 출력
 //4. 여기서 member.txt는 앞 실습에서 만든 회원 명부
 
 #include <iostream>
-#include <vector>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-// 회원 로그인 함수
-bool login(string username, string password) 
+void login(const string& username, const string& password) 
 {
-    ifstream file("member.txt"); // member.txt 파일 열기
+    ifstream file("member.txt");  // member.txt 파일에서 회원 명부 읽기
+
 
     if (file.fail())
     {
         cout << "파일을 열 수 없습니다." << endl;
-        return false;
+        return;
     }
 
     string line;
-    while (getline(file, line)) 
-    {
-        size_t pos = line.find(' '); // 이름과 비밀번호는 공백으로 구분되어 있음
-        if (pos != string::npos) 
-        {
-            string name = line.substr(0, pos); // 공백 이전까지
-            string pw = line.substr(pos + 1); // 공백 이후
+    bool logInOK = false;
 
+    while (getline(file, line))  // 파일에서 한 줄씩 읽어와서 이름과 비밀번호 비교
+    {
+        stringstream ss(line);
+        string name, pw;
+
+        if (ss >> name >> pw) // 공백으로 이름과 비밀번호 파싱
+        {
             if (name == username && pw == password) 
             {
-                file.close();
-                return true; // 로그인 성공
+                cout << "로그인 성공!" << endl;
+                logInOK = true;
+                break;
             }
         }
     }
 
-    file.close();
+    if (!logInOK)
+    {
+        cout << "로그인 실패!" << endl;
+    }
 
-    return false; // 로그인 실패
+    file.close();
 }
 
 int main() 
 {
     string username, password;
 
+    // 사용자로부터 이름 입력 받기
     cout << "이름을 입력하세요: ";
-    cin >> username;
+    getline(cin, username);
 
+    // 사용자로부터 비밀번호 입력 받기
     cout << "비밀번호를 입력하세요: ";
-    cin >> password;
+    getline(cin, password);
 
-    if (login(username, password)) 
-    {
-        cout << "로그인 성공!" << endl;
-    } 
-
-    else 
-    {
-        cout << "로그인 실패!" << endl;
-    }
+    // 로그인 시도
+    login(username, password);
 
     return 0;
 }
